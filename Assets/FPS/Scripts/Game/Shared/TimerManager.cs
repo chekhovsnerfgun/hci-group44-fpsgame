2d591a1117ecc75666073a6ecb99172ef1bf3f34
Lake Evers <lake.evers@ru.nl>
2024-07-05 10:27:16 +0200

    apparently these weren't in yet?

---
+++ Assets/FPS/Scripts/Game/Shared/TimerManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Unity.FPS.Game
+{
+    public class Timer : MonoBehaviour
+    {
+        public float timeRemaining = 120;
+        public bool timerIsRunning = false;
+        public Text timeText;
+
+        private void Start()
+        {
+            timerIsRunning = true;
+        }
+
+        void Update()
+        {   
+            if (timerIsRunning)
+            {
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                    DisplayTime(timeRemaining);
+                }
+                else
+                {
+                    Debug.Log("timer done");
+                    timeRemaining = 0;
+                    timerIsRunning = false;
+                }
+            }
+        }
+
+        void DisplayTime(float timeToDisplay)
+        {
+            timeToDisplay++;
+
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+}
